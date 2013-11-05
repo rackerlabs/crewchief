050180238aa02837476430c32c431c872f0e8033
carl.george <carl.george@rackspace.com>
2013-11-05 11:55:28 -0600

    use dictionary of settings

--- crewchief.py
+++ crewchief.py
@@ -24,32 +24,48 @@ import ConfigParser
 
 
 def parse_config():
+    ''' Obtain the user settings from /etc/crewchief/crewchief.conf. '''
+    # parse the config file
     config = ConfigParser.ConfigParser()
     config.read('/etc/crewchief/crewchief.conf')
+    # quit if the file is malformed or missing
+    if 'settings' not in config.sections():
+        syslog.syslog('malformed or missing configuration file')
+        sys.exit(1)
+    # set the defaults
     settings = {}
-    settings['max_api_attempts'] = config.get('main', 'max_api_attempts')
-    settings['api_wait_seconds'] = config.get('main', 'api_wait_seconds')
+    settings['max_api_attempts'] = 10
+    settings['api_wait_seconds'] = 60
+    # overwrite the defaults with values from config file
+    for each in ['max_api_attempts', 'api_wait_seconds']:
+        try:
+            settings[each] = config.get('settings', each)
+        except ConfigParser.NoOptionError:
+            pass
+    # return our settings dictionary
     return settings
 
 
 def get_datacenter():
+    ''' Obtain the datacenter from the xenstore. '''
     xencmd = ['xenstore-read', 'vm-data/provider_data/region']
     datacenter = subprocess.check_output(xencmd).rstrip('\n')
     return datacenter
 
 
-def query_api():
+def query_api(settings):
+    '''  '''
     apiurl = 'https://{DC}.{DOMAIN}/{VERSION}/{INFO}'.format(
         DC=get_datacenter(),
         DOMAIN='api.rackconnect.rackspace.com',
         VERSION='v1',
         INFO='automation_status')
-
     for each in range(settings.get('max_api_attempts')):
         try:
             rcstatus = requests.get(apiurl, timeout=3).text
         except requests.exceptions.Timeout:
-            syslog.syslog('rackconnect API call timeout, sleeping 60 seconds')
+            syslog.syslog('rackconnect API call timeout, '
+                          'sleeping 60 seconds')
             time.sleep(settings.get('api_wait_seconds'))
             continue
         else:
@@ -62,11 +78,12 @@ def query_api():
                 time.sleep(settings.get('api_wait_seconds'))
                 continue
     else:
-        syslog.syslog('hit maximum api attempts, giving up')
+        syslog.syslog('hit max api attempts, giving up')
         return False
 
 
-def call_tasks():
+def get_tasks(settings):
+    '''  '''
     tasks_dir = '/etc/crewchief/tasks.d'
     scripts = glob.glob('{}/*'.format(tasks_dir))
     try:
@@ -74,14 +91,21 @@ def call_tasks():
     except ValueError:
         pass
     scripts.sort()
+    return scripts
 
+
+def call_tasks(scripts):
+    '''  '''
     for script in scripts:
         try:
-            subprocess.call([script])
+            subprocess.check_call(script)
         except OSError:
             syslog.syslog('skipping non-executable script {}'.format(script))
+        except subprocess.CalledProcessError as e:
+            syslog.syslog('script {} exited with a status of {}'.format(
+                script, e.returncode))
         else:            
-            syslog.syslog('running executable script {}'.format(script))
+            syslog.syslog('successfully ran script {}'.format(script))
     else:
         syslog.syslog('completed all tasks')
 
@@ -89,7 +113,8 @@ def call_tasks():
 def main():
     settings = parse_config()
     if query_api(settings):
-        call_tasks(settings)
+        scripts = get_tasks(settings)
+        call_tasks(scripts)
     else:
         sys.exit(1)
 
