2db1fbc77345803a3f259fce928b7548d2f3a940
carl.george <carl.george@rackspace.com>
2013-11-11 09:20:13 -0600

    put os files into data_files subdir

--- setup.py
+++ setup.py
@@ -17,9 +17,9 @@ setuptools.setup(
         ]
     },
     data_files=[
-        ('/etc/crewchief', ['osfiles/crewchief.ini']),
-        ('/etc/crewchief/tasks.d', ['osfiles/10-example']),
-        ('/etc/init', ['osfiles/crewchief.conf'])
+        ('/etc/crewchief', ['data_files/crewchief.ini']),
+        ('/etc/crewchief/tasks.d', ['data_files/10-example']),
+        ('/etc/init', ['data_files/crewchief.conf'])
     ]
     classifiers=[
         'Programming Language :: Python',
