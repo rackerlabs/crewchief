afe8c06848164fb6f7c82a38f298119384a6b037
cgtx <carl@carlgeorge.us>
2014-01-06 16:05:45 -0600

    use unique upstart jobs for red hat and ubuntu

--- setup.py
+++ setup.py
@@ -1,19 +1,40 @@
 import os
+import platform
 import setuptools
+import sys
 
 
 def is_exe(fpath):
     return os.path.isfile(fpath) and os.access(fpath, os.X_OK)
 
+def get_distro():
+    return platform.linux_distribution()[0].lower()
+
 files = [('/etc/crewchief.d', ['data_files/crewchief.d/10-example'])]
 if is_exe('/sbin/initctl'):
     # upstart
-    f = ('/etc/init', ['data_files/upstart/crewchief.conf'])
-    files.append(f)
+    distro = get_distro()
+    if 'ubuntu' in distro:
+        print('Detected distro as Ubuntu.')
+        f = ('/etc/init', ['data_files/upstart-ubuntu/crewchief.conf'])
+        files.append(f)
+    elif 'centos' in distro:
+        print('Detected distro as CentOS.')
+        f = ('/etc/init', ['data_files/upstart-redhat/crewchief.conf'])
+        files.append(f)
+    elif 'red hat' in distro:
+        print('Detected distro as Red Hat.')
+        f = ('/etc/init', ['data_files/upstart-redhat/crewchief.conf'])
+        files.append(f)
+    else:
+        sys.exit('Unrecognized upstart system.')
 elif is_exe('/usr/lib/systemd/systemd'):
     # systemd
+    print('Detected systemd init system.')
     f = ('/usr/lib/systemd/system', ['data_files/systemd/crewchief.service'])
     files.append(f)
+else:
+    sys.exit('Unsupported distro.')
 
 setuptools.setup(
     name='crewchief',
